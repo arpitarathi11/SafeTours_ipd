8a0dc15bd571bafa4922c27abaac07234c568025
sakshi3861 <sakshisushilshelar@gmail.com>
2026-06-29 12:01:39 +0530

    Installed react native

--- test_connection.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-from firebase_admin import credentials, firestore
-cred = credentials.Certificate("serviceAccountKey.json")
-app = firebase_admin.initialize_app(cred)
-db = firestore.client(app, database_id="default")
-print("Client created, trying write...")
-db.collection("connection_test").document("ping").set({"status": "ok"})
-print("SUCCESS - Firestore connection works!")
